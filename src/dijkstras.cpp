575aa15ba250be3cacbe72d52e693137b4bafb39
Briana Truong <brianaht@uci.edu>
2025-03-17 05:57:54 +0000

    Changed empty path check condition

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -65,8 +65,8 @@ vector<int> extract_shortest_path(const vector<int>& distances, const vector<int
 }
 
 void print_path(const vector<int>& v, int total) {
-    if (v.empty()) {
-        cout << "\nNo path exists." << endl;
+    if (total == INF) {
+        cout << "No path exists." << endl;
         return;
     }
 
